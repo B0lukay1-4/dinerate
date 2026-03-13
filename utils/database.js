95b4bc10882afb095c5ddbcae60b4060b41bc5dd
Bmdev <boluwatifesotayokay@gmail.com>
2026-03-13 07:47:24 -0700

    receive modify from dell 1

--- utils/database.js
+++ utils/database.js
@@ -9,19 +9,19 @@ export const connectToDB = async () => {
     console.log("MongoDB is already connected");
     return;
   }
-// if not connected we try to establish the connecttion
+  // if not connected we try to establish the connecttion
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-// options object
-dbNmae:"dine_rate",
-useNewUrlParser:true,
-useUnifiedTopology:true,
+      // options object
+      dbNmae: "dine_rate",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     // if connected correctly we set isConnected to true
 
-    isConnected =true;
+    isConnected = true;
     console.log("MongoDB connected");
   } catch (error) {
-console.log(error);
+    console.log(error);
   }
 };
